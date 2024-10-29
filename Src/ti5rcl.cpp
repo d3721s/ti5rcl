91e2d8f7092b6f6d87ed5f64ed158a212dc7d632
D3721s <88261082+d3721s@users.noreply.github.com>
2024-10-29 01:26:06 -0700

    init

--- Src/ti5rcl.cpp
+++ Src/ti5rcl.cpp
@@ -51,10 +51,8 @@ bool ti5Robot::linear_move(const KDL::Frame *end_pos)
     double v;
     for (int i = 0; i < _nrOfJoints; i++)
     {
-
         _joint[i]->quickGetCSP(&c, &v, &qNow(i));
         qNow(i) = 0;
-#warning
     }
     // 求末端位姿
     try
@@ -63,25 +61,11 @@ bool ti5Robot::linear_move(const KDL::Frame *end_pos)
         ChainFkSolverPos_recursive fwdkin(_chain);
         fwdkin.JntToCart(qNow, frameNow);
         tlog_info << "frameNow: " << frameNow.p.x() << "," << frameNow.p.y() << "," << frameNow.p.z() << endl;
-
-        // Path
-        // Path_Circle
-        // Path_Composite
-        // Path_Cyclic_Closed
-        // Path_Line
-        // Path_Point
-        // Path_RoundedComposite
-
         Path_Line *path = new Path_Line(frameNow, *end_pos, new RotationalInterpolation_SingleAxis(), 0.2);
-
-        // Trajectory defines a motion of the robot along a path.
-        // This defines a trapezoidal velocity profile.
         VelocityProfile *velpref = new VelocityProfile_Trap(0.5, 0.1);
         velpref->SetProfile(0, path->PathLength());
         Trajectory *traject = new Trajectory_Segment(path, velpref);
         Trajectory_Composite *ctraject = new Trajectory_Composite();
-
-        // use the trajectory
         double dt = 0.1;
         std::ofstream of("./trajectory.dat");
         for (double t = 0.0; t <= traject->Duration(); t += dt)
@@ -92,14 +76,12 @@ bool ti5Robot::linear_move(const KDL::Frame *end_pos)
                 for (int j = 0; j < 4; ++j)
                     of << current_pose(i, j) << "\t";
             of << "\n";
-            // also velocities and accelerations are available !
-            // traject->Vel(t);
-            // traject->Acc(t);
         }
         of.close();
     }
     catch (Error &error)
     {
+
         std::cout << "I encountered this error : " << error.Description() << std::endl;
         std::cout << "with the following type " << error.GetType() << std::endl;
     }
